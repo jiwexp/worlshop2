06ad220a07ee6cd16f4798514bd246ffd9aa8dc3
jiwexp <tanapong.jiwe@gmail.com>
2021-01-10 14:33:53 +0700

    add name variable

---
+++ variable/name.py
@@ -0,0 +1,15 @@
+#สามารถประกาศได้
+myvar = "John Cena"
+my_var = "John Cena"
+_my_var = "John Cena"
+myVar + "John Cena"
+MYVAR = "Jphn Cena"
+myvarx = "John Cena"
+
+#ไม่สามารถประกาศได้
+2myvar = "John Cena"
+my-var = "John Cena"
+my var = "John Cena"
+
+
+
