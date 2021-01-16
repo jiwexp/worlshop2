44d2acad7008aada92234b6ec2e6e7f572434433
jiwexp <tanapong.jiwe@gmail.com>
2021-01-16 14:34:33 +0700

    string file

---
+++ String/String_Fomat.py
@@ -0,0 +1,6 @@
+age = 26
+txt = " My name is jiw, and I'm {}"
+
+result = txt.format(age)
+
+print("result", result)
---
+++ String/f_String.py
@@ -0,0 +1,6 @@
+name = "jiw"
+age = 26
+
+result = f" My name is {name}, and I'm {age}"
+
+print("result", result)
