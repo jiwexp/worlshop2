6f7ae321e1dce88caa116fe32964d2fa90c7cd9d
jiwexp <tanapong.jiwe@gmail.com>
2021-01-17 15:39:20 +0700

    Tuples Access Join Unpack

---
+++ Tuples/Access Tuple items.py
@@ -0,0 +1,15 @@
+thislist = [
+    "apple",
+    "banana",
+    "cherry",
+]  # Tuple เหมือนลิส แต่ Tuple ไม่สามารถเปลี่ยนแปลงค่าได้
+
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
+
+print(thislist[-1])
+print(thislist[-2])
+print(thislist[-3])
+
+print(thislist[1:])
