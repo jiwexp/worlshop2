f62d3a5cecf3ed57314bc1b95f13a2646abc1761
jiwexp <tanapong.jiwe@gmail.com>
2021-02-12 17:48:46 +0700

    IF ELSE

---
+++ Python If … Else/IF … Else3.py
@@ -0,0 +1,8 @@
+a = 200
+b = 33
+if b > a:
+    print("b is greter than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is grether than b")
---
+++ Python If … Else/IF … Else4.py
@@ -0,0 +1,6 @@
+a = 200
+b = 33
+if b > a:
+    print("b is greter than a")
+else:
+    print("a is grether than b")
