67d5a2926788ba0fffe5238faf0a3e8bca45bf38
jiwexp <tanapong.jiwe@gmail.com>
2021-02-26 00:58:21 +0700

    Module Python

---
+++ Python Module/Variables/Function/calculate.py
@@ -0,0 +1,11 @@
+def add(number1, number2):
+    return number1 + number2
+
+def multipled(number1, number2)
+     return number1 * number2
+
+def substract(number1, number2):
+    return number1 - number2
+
+def divide(number1, number2):
+    return number1 / number2
---
+++ Python Module/Variables/Function/run.daction.py
@@ -0,0 +1,9 @@
+import calculate
+
+number = 10
+number = 2
+
+print(calculate.add(10, 2))
+print(calculate.sunstract(10, 2))
+print(calculate.multiplied(10, 2))
+print(calculate.divide(10, 2))
